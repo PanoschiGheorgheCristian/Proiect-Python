29d1e8d08eb51ed7b6fbda5c2c41a35185d360ed
PanoschiGheorgheCristian <79131710+PanoschiGheorgheCristian@users.noreply.github.com>
2023-01-09 05:01:40 +0200

    Cleaned up code and documented it
    Also added the main

--- TrapTheMouse.py
+++ TrapTheMouse.py
@@ -1,4 +1,4 @@
-import pygame, numpy as np, sys, math, random
+import pygame, sys, math, random
 from hexagon import Hexagon
 
 pygame.init()
@@ -17,9 +17,17 @@ HEXRADIUS = 30
 HEXMINIMALDISTANCE = HEXRADIUS / 2 * math.sqrt(3)
 
 def get_index_of_neighbours(index):
+    '''
+    This function takes the index of an element on the board and returns the indexes of adjacent elements.
+    
+    :param index: The index of the element whose neighbours we are looking for
+    :return: The indexes of the neighbours, returned as a tuple of 6. The order of the neighbours is: up-left, left, down-left, down-right, right, up-right
+    '''
+    
     left = index - 1
     right = index + 1
-    if index % 22 < 11:
+    
+    if index % 22 < 11: #Note that the extra cases are because the hexagons are intertwined horizontally, so computation has to be done on even/odd rows separately
         up_left = index - 12
         up_right = index - 11
         down_left = index + 10
@@ -46,6 +54,16 @@ def get_index_of_neighbours(index):
     return up_left, left, down_left, down_right, right, up_right
 
 def get_moves(board, index):
+    '''
+    This function finds what moves are available on a given goard from the given position. 
+    
+    If the move is equal to -2 it means the tile is an exit tile, at the edge of the map; if the move is equal to -1 the tile to move on is broken and therefore inacessible.
+    
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param index: The index of the hexagon we are currently on
+    :return: returns a tuple of 6 moves, each with value: -2 if outside the board, -1 if tile is broken, int in [0, 120] otherwise, representing the index of the hexagon to move onto. 
+    '''
     moves = list(get_index_of_neighbours(index))
     
     for index in range(6):
@@ -55,7 +73,12 @@ def get_moves(board, index):
     return moves            
     
 
-def initialize_board(board):
+def initialize_board(board) -> None:
+    '''
+    This function adds Hexagon elements to the board, which will serve as the main data structure.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    '''
     for index1 in  range(11):
         for index2 in range(11):
             if index1 % 2 == 0:
@@ -63,14 +86,25 @@ def initialize_board(board):
             else:
                 board.append(Hexagon(100 + HEXMINIMALDISTANCE + index2 * HEXMINIMALDISTANCE * 2, 100 + 1.5 * HEXRADIUS + (index1 - 1) * 1.5 * HEXRADIUS, 0))
 
-def initialize_broken_tiles(board):
+def initialize_broken_tiles(board) -> None:
+    '''
+    This function randomly generates between 10 and 15 broken tiles on the map. Tile 60, the spawn point of the mouse, is evaded.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    '''
     broken_tiles_nr = random.randint(10,15)
     for index in range(broken_tiles_nr):
         tile = random.randint(0, 120)
         if tile != 60:
             board[tile].destroy_hex()
             
-def render(board, screen):
+def render(board, screen) -> None:
+    '''
+    This function paints on the screen the current state of the board and the mouse, as well as highlights the tile the cursor is on if it is not broken.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param screen: The screen that pygame uses to paint onto
+    '''
     clock.tick(60)
 
     screen.fill(BACKGROUND)
@@ -82,7 +116,10 @@ def render(board, screen):
     
     pygame.display.update()
 
-def lose_game():
+def lose_game() -> None:
+    '''
+    The function that paints the final screen when the game has ended, with the mouse escaping.
+    '''
     global font
     global game_mode
     if game_mode == 0:
@@ -97,7 +134,10 @@ def lose_game():
     pygame.quit()
     quit()
 
-def win_game():
+def win_game() -> None:
+    '''
+    The function that paints the final screen when the game has ended, with the mouse being trapped.
+    '''
     global font
     global game_mode
     if game_mode == 0:
@@ -112,7 +152,14 @@ def win_game():
     pygame.quit()
     quit()
 
-def move_by_player(board, moves):
+def move_by_player(board, moves) -> int:
+    '''
+    This function lets the other human player move the mouse to an adjacent available position.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param moves: The available moves the player can make, in a tuple of 6 format
+    :return: the index of the hexagon the player moves onto
+    '''
     while 1:
         coursor_x, coursor_y = pygame.mouse.get_pos()
         render(board, screen)
@@ -124,14 +171,30 @@ def move_by_player(board, moves):
                             move_to = move
                             return move_to
 
-def move_by_c1(moves):
+def move_by_c1(moves) -> int:
+    '''
+    This function determines the move the first difficulty of the computer will make. It is random from the available moves.
+    
+    :param moves: The available moves the player can make, in a tuple of 6 format
+    :return: the index of the hexagon the player moves onto
+    '''
     move_to = moves[random.randint(0,5)]
     while move_to == -1:
         move_to = moves[random.randint(0,5)]
         
     return move_to
 
-def move_by_c2(board, mouse_index):
+def move_by_c2(board, mouse_index) -> int:
+    '''
+    This function determines what move will the second difficulty of the computer make. It first does a BFS to find the nearest exit point and stores it in end_point.
+    It then estimates the value of each of the 6 moves based on wheter or not it brings the mouse closer to the end_point.
+    Illegal moves have a default value of -999 in order to not ever be selected. 
+    If the mouse has no legal moves the game ends before requiring it to move.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param mouse_index: The index of the hexagon the mouse is currently on
+    :return: The tile the mouse moves onto
+    '''
     moves = get_moves(board, mouse_index)
     
     global visited_nodes
@@ -165,8 +228,6 @@ def move_by_c2(board, mouse_index):
     
     end_point = visited_nodes[len(visited_nodes) - 1]
     
-    print(end_point)
-    
     moves = get_moves(board, mouse_index)
     move_value = [0,0,0,0,0,0]
     
@@ -175,7 +236,8 @@ def move_by_c2(board, mouse_index):
         if moves[index] == -1:
             move_value[index] = -999
     
-    end_point_x = end_point % 11 + 1
+    #Gets the ends of the path in a more manageable form, two indexes in a 11x11 matrix of hexagons for each.
+    end_point_x = end_point % 11 + 1 
     end_point_y = (end_point - end_point % 11) // 11 + 1
     mouse_x = mouse_index % 11 + 1
     mouse_y = (mouse_index - mouse_index % 11) // 11 + 1
@@ -187,11 +249,20 @@ def move_by_c2(board, mouse_index):
     move_value[1] = move_value[1] + ( mouse_x - end_point_x )
     move_value[4] = move_value[4] + ( end_point_x - mouse_x )
     
-    print(move_value)
-    print(moves[move_value.index(max(move_value))])
     return moves[move_value.index(max(move_value))]
 
-def move_by_c3(board, mouse_index):
+def move_by_c3(board, mouse_index) -> int:
+    '''
+    This function determines what move will the third difficulty of the computer make. It first does a BFS to find the nearest exit point and stores it in end_point.
+    It then estimates the value of each of the 6 moves based on wheter or not it brings the mouse closer to the end_point.
+    If any move would result in the mouse entering an alcove (surrounded by 4 adjacent broken tiles), the mouse evades it and moves a different way.
+    Illegal moves have a default value of -999 in order to not ever be selected. 
+    If the mouse has no legal moves the game ends before requiring it to move.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param mouse_index: The index of the hexagon the mouse is currently on
+    :return: The tile the mouse moves onto
+    '''
     moves = get_moves(board, mouse_index)
     
     global visited_nodes
@@ -225,17 +296,16 @@ def move_by_c3(board, mouse_index):
     
     end_point = visited_nodes[len(visited_nodes) - 1]
     
-    print(end_point)
-    
     moves = get_moves(board, mouse_index)
     move_value = [0,0,0,0,0,0]
     
+    #This is where we check ahead for alcoves. The value is -900 so that it may be preffered over an illegal move.
     for index_0 in range(6):
         if moves[index_0] != -1:
             moves_next_turn = get_moves(board, moves[index_0])
             for index in range(6):
                 if moves_next_turn[index] == -1 and moves_next_turn[(index + 1) % 6] == -1 and moves_next_turn[(index + 2) % 6] == -1 and moves_next_turn[(index + 3) % 6] == -1:
-                    move_value[index_0] = -999
+                    move_value[index_0] = -900
     
     for index in range(6):
         if moves[index] == -1:
@@ -252,12 +322,17 @@ def move_by_c3(board, mouse_index):
     move_value[5] = move_value[5] + ( end_point_x - mouse_x ) / 2 + ( mouse_y - end_point_y )
     move_value[1] = move_value[1] + ( mouse_x - end_point_x )
     move_value[4] = move_value[4] + ( end_point_x - mouse_x )
-    
-    print(move_value)
-    print(moves[move_value.index(max(move_value))])
+
     return moves[move_value.index(max(move_value))]
             
-def move_mouse(board, game_mode):
+def move_mouse(board, game_mode) -> None:
+    '''
+    This funtion is responsible for checking the moves of the mouse (thereby checking the win/lose condition as well) and computing what entity will make the move.
+    Once the move is received, it updates the board with the new information.
+    
+    :param board: This is the board, a list of Hexagon class objects
+    :param game_mode: The selected game mode as the user requested at command line. It is 0 for human, and 1/2/3 for computer of difficulty 1/2/3 respectively.
+    '''
     global mouse_index
     global mouse_position
     moves = get_moves(board, mouse_index)
@@ -285,53 +360,54 @@ def move_mouse(board, game_mode):
     mouse_position = (board[move_to].center[0] - 20, board[move_to].center[1] - 20)
     mouse_index = move_to
     
+if __name__ == '__main__':
+    GameRunning = True
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+    pygame.display.set_caption('Trap the Mouse')
+    font = pygame.font.SysFont('arial', 64)
 
-GameRunning = True
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-pygame.display.set_caption('Trap the Mouse')
-font = pygame.font.SysFont('arial', 64)
+    #Selecting game_mode based on user console input
+    argument = sys.argv[1] 
+    if argument == "human":
+        game_mode = 0
+    elif argument == "computer1":
+        game_mode = 1
+    elif argument == "computer2":
+        game_mode = 2
+    elif argument == "computer3":
+        game_mode = 3
+    else:
+        print("Argument in not adequate; try human for a human opponent, or computer1-3 for an AI opponent.")
+        quit()
 
-argument = sys.argv[1]
-if argument == "human":
-    game_mode = 0
-elif argument == "computer1":
-    game_mode = 1
-elif argument == "computer2":
-    game_mode = 2
-elif argument == "computer3":
-    game_mode = 3
-else:
-    print("Argument in not adequate; try human for a human opponent, or computer1-3 for an AI opponent.")
-    quit()
-
-board = []
-initialize_board(board)
-initialize_broken_tiles(board)
-exits = [0,1,2,3,4,11,22,33,44,55,66,77,88,99,110,111,112,113,114,115,116,117,118,119,120,109,98,87,76,65,54,43,32,21,10,9,8,7,6,5]
-visited_nodes = []
-nodes_to_visit = []
-
-mouse_image = pygame.image.load("Assets/mouse.png").convert_alpha()
-mouse_position = (board[60].center[0] - 20, board[60].center[1] - 20)
-mouse_index = 60
-board[mouse_index].has_mouse = 1
-
-while GameRunning:
-    coursor_x, coursor_y = pygame.mouse.get_pos()
-    player_turn = 1
+    board = []
+    initialize_board(board)
+    initialize_broken_tiles(board)
     
-    for event in pygame.event.get():
+    visited_nodes = [] #Used for BFS
+    nodes_to_visit = []
+
+    mouse_image = pygame.image.load("Assets/mouse.png").convert_alpha()
+    mouse_position = (board[60].center[0] - 20, board[60].center[1] - 20)
+    mouse_index = 60
+    board[mouse_index].has_mouse = 1
+
+    while GameRunning:
+        coursor_x, coursor_y = pygame.mouse.get_pos()
+        player_turn = 1
         
-        if event.type == pygame.QUIT:
-            GameRunning = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for hex in board:
-                if hex.inside_point(coursor_x, coursor_y) and hex.has_mouse == 0 and hex.destroyed == 0:
-                    hex.destroy_hex()
-                    player_turn = 0
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                GameRunning = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for hex in board:
+                    if hex.inside_point(coursor_x, coursor_y) and hex.has_mouse == 0 and hex.destroyed == 0:
+                        hex.destroy_hex()
+                        player_turn = 0
 
-    if player_turn == 0:
-        move_mouse(board, game_mode)
+        if player_turn == 0:
+            move_mouse(board, game_mode)
 
-    render(board, screen)    
+        render(board, screen)    
--- hexagon.py
+++ hexagon.py
@@ -6,6 +6,48 @@ HIGHLIGHTEDTILE = (82,97,82)
 DESTROYEDTILE = (0,0,0)
 
 class Hexagon:
+    '''
+    A class to represent a Hexagon, or a tile from the board.
+    
+    ```
+    
+    Attributes
+    ``````````
+    radius : int
+        always equal to 30
+    position : tuple([float, float])
+        the coordinates of the point at the top of the hexagon
+    center : tuple([float, float])
+        the coordinates of the center of the hexagon
+    colour : tuple([int,int,int])
+        the RGB values of the default colour of the hexagon
+    highlight : tuple([int,int,int])
+        the RGB values of the colour the hexagon is when highlighted
+    destroyed : int
+        stores wether or not the hexagon is destroyed
+    has_mouse : int
+        stores wether or not the mouse is currently on top of the hexagon
+    minimal_radius : float
+        the distance from the center of the hexagon to any of its edges
+        
+    Methods
+    ```````
+    compute_vertices():
+        returns the coordinates of all the points of the hexagon, in order up, up-left, down-left, down, down-right, up-right
+        
+    destroy_hex():
+        sets the destroyed attribute to 1
+        
+    inside_point(coursor_x, coursor_y):
+        checks wether the given coordinates are inside of the hexagon
+        
+    update_higlight(coursor_x, coursor_y):
+        checks wether the given coordinates are inside the hexagon; 
+        if they are not it returns to normal colour, but if they are and the hexagon does not have the mouse on top of it, it becomes highlighted.
+        
+    render(screen):
+        paints the hexagon on the screen; if it is destroyed it will be black.
+    '''
     
     radius: int = 30
     position: tuple([float, float])
@@ -17,6 +59,13 @@ class Hexagon:
     minimal_radius: float = math.sqrt(3) * radius / 2
     
     def __init__(self, x, y, destroyed) -> None:
+        '''
+        Initializes all the values needed for the hexagon.
+        
+        :param x: x coordinate of the top of the hexagon
+        :param y: y coordinate of the top of the hexagon
+        :param destroyed: wether or not the given hexagon is destroyed
+        '''
         center_coordinates = x, y + self.radius
         self.position = x, y
         self.center = center_coordinates
@@ -26,6 +75,11 @@ class Hexagon:
             self.colour = DESTROYEDTILE
         
     def compute_vertices(self) -> List[Tuple[float, float]]:
+        '''
+        Returns the coordinates of all the points of the hexagon, in order up, up-left, down-left, down, down-right, up-right
+        
+        :return: a tuple of 6 elements, each the coordinates of a vertice of the hexagon
+        '''
         x, y = self.position
         half_radius = self.radius / 2
         minimal_radius = self.minimal_radius
@@ -39,18 +93,42 @@ class Hexagon:
         ]
     
     def destroy_hex(self) -> None:
+        '''
+        Sets the destroyed attribute of the hexagon to 1
+        '''
         self.destroyed = 1
     
-    def inside_point(self, cursor_x, cursor_y) -> bool:
-        return math.dist( (cursor_x, cursor_y), self.center ) < self.minimal_radius
+    def inside_point(self, coursor_x, coursor_y) -> bool:
+        '''
+        Checks wether the given coordinates are inside of the hexagon
+        
+        If the distance from the center of the hexagon to the coordinates is smaller than the minimal radius the point with the given coordinates is within the hexagon
+        
+        :param coursor_x: x coordinate of the point
+        :param coursor_y: y coordinate of the point
+        :return: wether or not the point is within the hexagon
+        '''
+        return math.dist( (coursor_x, coursor_y), self.center ) < self.minimal_radius
     
     def update_highlight(self, coursor_x, coursor_y) -> None:
+        '''
+        Checks wether the given coordinates are inside the hexagon; 
+        If they are not it returns to normal colour, but if they are and the hexagon does not have the mouse on top of it, it becomes highlighted.
+        
+        :param coursor_x: x coordinate of the point
+        :param coursor_y: y coordinate of the point
+        '''
         if self.inside_point(coursor_x, coursor_y) == False:
             self.colour = TILE
         if self.inside_point(coursor_x, coursor_y) and self.has_mouse == 0:
             self.colour = HIGHLIGHTEDTILE
     
     def render(self, screen) -> None:
+        '''
+        Paints the hexagon on the screen; if it is destroyed it will be black.
+        
+        :param screen: the screen used by Pygame to paint on
+        '''
         if self.destroyed == 0:
             pygame.draw.polygon(screen, self.colour, self.vertices)
             pygame.draw.polygon(screen, DESTROYEDTILE, self.vertices, 1)
